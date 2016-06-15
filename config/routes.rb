3dbbf1649716d5d942a472da3e9369f5125e21f3
Salman Ahmad <salman_sv@yahoo.com>
2016-06-15 20:43:17 +0000

    index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Webpage::Application.routes.draw do
+root 'places#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
